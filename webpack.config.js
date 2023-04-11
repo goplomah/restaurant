30b56695ec41ff1ccf0aa6bb09aa8fbb061ce722
Maks Subochev <maxim.subochev@yandex.ru>
2023-04-11 22:08:44 +0300

    feat: инициализация

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: './app/pages/index.js'
+  }   ,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+      // favicon: './app/images/favicons/faviconka.ico',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(), 
+    // new CopyPlugin({
+    //   patterns: [
+    //     {from: './app/images/favicons/faviconka.ico'},
+    //   ],
+    // }),
+  ]
+}
