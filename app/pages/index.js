f06be3daac132f6531558bc1365b5d218889bc0a
Maks Subochev <maxim.subochev@yandex.ru>
2023-04-16 21:32:07 +0300

    feat: добавить секции слайдер и галерея

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,1 +1,27 @@
 import  '../styles/index.scss';
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/autoplay';
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination, Autoplay],
+
+    pagination: {
+    el: ".swiper-pagination",
+    type: 'bullets',
+    },
+
+    autoplay: {
+      delay: 8000,
+      disableOnInteraction: false
+    },
+
+    effect: "fade",
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+});
