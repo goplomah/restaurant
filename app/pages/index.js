f587c3400f786b14b0621de5c6b11833b317d7ec
Maks Subochev <maxim.subochev@yandex.ru>
2023-04-17 20:24:45 +0300

    feat: адаптив хедера и первых 5ти секций

--- app/pages/index.js
+++ app/pages/index.js
@@ -6,6 +6,13 @@ import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import 'swiper/scss/autoplay';
 
+const menuBtn = document.querySelector('.header__menu-btn');
+const menu = document.querySelector('.header__menu-list');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle("header__menu-list_active");
+});
+
 const swiper = new Swiper('.swiper', {
     modules: [Navigation, Pagination, Autoplay],
 
